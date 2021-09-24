cb0fc63f9643d0aab716577a1e7e0b9f92796502
visnusantos1203 <visnu.santos1203@gmail.com>
2021-09-24 20:45:08 +0800

    configured necessary routes for controllers and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    confirmations: 'confirmations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
+  resources :portfolios, :payments, :wallets, :stocks
 end
