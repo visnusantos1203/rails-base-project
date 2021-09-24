554008e713ffa3b45e6ada86b4bf8b0661997570
visnusantos1203 <visnu.santos1203@gmail.com>
2021-09-24 20:50:27 +0800

    added necessary models for the app and corresponding validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_09_22_122844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "payments", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "wallet_id"
+    t.decimal "quantity"
+    t.integer "stock_id"
+    t.decimal "current_price"
+    t.datetime "time_placed"
+    t.string "transaction_type"
+  end
+
+  create_table "payments_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "payment_id", null: false
+    t.index ["payment_id", "user_id"], name: "index_payments_users_on_payment_id_and_user_id"
+    t.index ["user_id", "payment_id"], name: "index_payments_users_on_user_id_and_payment_id"
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "company_name"
+    t.string "symbol"
+    t.string "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "user_type"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
 end
