73bc44042cf7190733a9ba2353c7af4287e3ac94
visnusantos1203 <visnu.santos1203@gmail.com>
2021-09-24 20:53:31 +0800

    Fetched stocks data and saved it to Stock model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+client = IEX::Api::Client.new(
+    publishable_token: 'Tpk_01273dfe6905454eb34261aa36cd7198',
+    secret_token: 'Tsk_c3bab9f09d4440539a75b1803a0f83a4',
+    endpoint: 'https://sandbox.iexapis.com/v1'
+  )
+all_symbols = client.ref_data_symbols
+
+Stock.destroy_all
+
+500.times do |index|
+  company_info = client.company(all_symbols[index].symbol)
+
+  begin
+    Stock.create(symbol: all_symbols[index].symbol, company_name: company_info.company_name, description: company_info.description)
+  rescue  StandardError
+    nil
+  end
+end
