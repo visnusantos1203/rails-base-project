15828a15d61f3433993d5a4c2afc88352059b3b0
visnusantos1203 <visnu.santos1203@gmail.com>
2021-10-02 13:14:47 +0800

    Added an Admin MVC. The admin controll has the ability to controll everything in the app

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,68 @@
+class AdminsController < ApplicationController
+  before_action :authenticate_admin!
+  def dashboard
+    @for_approvals = User.where(approved: false)
+    @all_users = User.all
+    @transactions = Payment.all
+  end
+
+  def approve_users;
+    @user = User.find(params[:id])
+    @user.approved = true
+    @user.save
+    if @user.save
+      ConfirmationMailer.account_approval_email(@user.email).deliver_now
+      redirect_back fallback_location: admins_dashboard_path
+    else
+      render :approve_users
+    end
+  end
+  
+  def all_transactions;
+    @payments = Payment.all
+  end
+
+  def new_user;
+    @new_user = User.new
+  end
+
+  def create_user;
+    @create_user = User.new(params.require(:user).permit(:first_name, :last_name, :username, :email, :password))
+    @create_user.approved = true
+    @create_user.wallet_id = @create_user.id
+
+    if @create_user.save
+      redirect_to root_path
+    else
+      redirect_back fallback_location: admins_dashboard_path
+    end
+  end
+
+  def show_user;
+    @user = User.find(params[:id])
+  end
+
+  def edit_user;
+    @user = User.find(params[:id])
+  end
+
+  def update_user;
+    @user = User.find(params[:id])
+    @user.update(update_params)
+    @user.save
+    if @user.save
+      redirect_back fallback_location: admins_dashboard_path
+    else
+      redirect_to :edit_user
+    end
+
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email)
+  end
+
+
+end
---
+++ app/models/admin.rb
@@ -0,0 +1,9 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :username, :email, :password, presence: true
+
+end
