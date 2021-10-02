e0b8e9688f3329a1d3b378ef4f4f7c708a8e1587
visnusantos1203 <visnu.santos1203@gmail.com>
2021-10-02 13:03:49 +0800

    Configured User model and added necessary callback functions for mailer and user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ApplicationRecord
+  has_many :payments
+  has_one :wallet, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
-  # validates :first_name, :last_name, :username, :user_type, presence: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  validates :first_name, :last_name, :username, presence: true
+  after_save :create_user_wallet, :send_welcome_email
 
-  # has_and_belongs_to_many :roles
-  has_many :payments
-  has_many :wallets
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  private 
+
+  def send_welcome_email
+    WelcomeMailer.welcome_email(email).deliver_now if approved == false
+    WelcomeMailer.welcome_email(email).deliver_now if approved == true
+  end
+
+  def create_user_wallet
+    user_wallet = Wallet.create(user_id: User.last.id, balance: 100_000)
+  end
+
 end
