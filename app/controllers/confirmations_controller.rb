6b3ef08412d0fe0470bcdaf8614586d1e9e759d4
visnusantos1203 <visnu.santos1203@gmail.com>
2021-10-02 12:37:12 +0800

    Deleted unnecessary confirmation controllers

--- app/controllers/confirmations_controller.rb
+++
@@ -1,8 +0,0 @@
-class ConfirmationsController < Devise::ConfirmationsController
-  private
-
-  def after_confirmation_path_for(_resource_name, resource)
-    sign_in(resource)
-    root_path # redirect to root path or wherever it should be redirected
-  end
-end
