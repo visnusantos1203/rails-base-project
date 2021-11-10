922c274a8bf096ef23cb75872e65d64984f117c9
visnusantos1203 <visnu.santos1203@gmail.com>
2021-11-10 12:49:25 +0800

    Created price_history column in Stock and added logic for storing historical_prices in the seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_104224) do
+ActiveRecord::Schema.define(version: 2021_11_08_142735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_10_01_104224) do
     t.string "symbol"
     t.string "description"
     t.string "logo_url"
+    t.string "price_history"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,24 @@
-client = IEX::Api::Client.new(
-    publishable_token: 'Tpk_01273dfe6905454eb34261aa36cd7198',
-    secret_token: 'Tsk_c3bab9f09d4440539a75b1803a0f83a4',
-    endpoint: 'https://sandbox.iexapis.com/v1'
-  )
-all_symbols = client.ref_data_symbols
-
+# Populate Stock model with company details
 Stock.destroy_all
+
+client = CallApi.call
+
+all_symbols = client.ref_data_symbols
 
 500.times do |index|
   company_info = client.company(all_symbols[index].symbol)
+  historical_prices = client.historical_prices(all_symbols[index].symbol)
 
   begin
-    Stock.create(symbol: all_symbols[index].symbol, company_name: company_info.company_name, description: company_info.description, logo_url: "https://storage.googleapis.com/iex/api/logos/#{all_symbols[index].symbol}.png")
+    new_stock = Stock.new(symbol: all_symbols[index].symbol, 
+                          company_name: company_info.company_name,
+                          description: company_info.description,
+                          logo_url: "https://storage.googleapis.com/iex/api/logos/#{all_symbols[index].symbol}.png")
+    price_history = new_stock.price_history
+    historical_prices.each do |price|
+      price_history.push(price.close)
+    end
+    new_stock.save
   rescue  StandardError
     nil
   end
