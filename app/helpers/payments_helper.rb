22b389edf5f6e589cbe983da777df30f36ef7cd5
visnusantos1203 <visnu.santos1203@gmail.com>
2021-11-10 12:43:07 +0800

    Used CallApi service object

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -4,11 +4,7 @@ module PaymentsHelper
   end
   
   def get_price(index)
-    client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_01273dfe6905454eb34261aa36cd7198',
-      secret_token: 'secret_token',
-      endpoint: 'https://sandbox.iexapis.com/stable'
-    )
+    client = CallApi.call
     stocks = Stock.all
     @price = client.price(stocks[index].symbol)
   end
