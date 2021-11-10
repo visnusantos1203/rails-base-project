e43c94e2241653d925a209e1639bb9370a3ff7ae
visnusantos1203 <visnu.santos1203@gmail.com>
2021-11-10 12:23:23 +0800

    Added dynamic monthly price chart for each company in the market list

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,24 +5,10 @@ class StocksController < ApplicationController
   end
 
   def show
-   @stock = Stock.find(params[:id])
-   
-   client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_01273dfe6905454eb34261aa36cd7198',
-      secret_token: 'Tsk_c3bab9f09d4440539a75b1803a0f83a4',
-      endpoint: 'https://sandbox.iexapis.com/stable'
-    )
-    @data_keys = []
-    @data_values = []
+    client = CallApi.call
+
+    @stock = Stock.find(params[:id])
     @price = client.price(@stock.symbol)
-
-    historical_prices = client.historical_prices(@stock.symbol)
-
-    historical_prices.each do |price|
-      @data_keys << price.label
-      @data_values << price.close
-    end
-
     @news = client.news(@stock.symbol)
   end
 end
