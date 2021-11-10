9da5e1a0b340958851782a6d72c6a2e795a0fba0
visnusantos1203 <visnu.santos1203@gmail.com>
2021-11-10 12:25:18 +0800

    Create a helper for stock feature that fetches the data from API that will be used for chart

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,23 +1,31 @@
 module StocksHelper
   def change_percentage(symbol)
-    client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_01273dfe6905454eb34261aa36cd7198',
-      secret_token: 'Tsk_c3bab9f09d4440539a75b1803a0f83a4',
-      endpoint: 'https://sandbox.iexapis.com/stable'
-    )
+    client = CallApi.call
   
     historical_price = client.historical_prices(symbol)
     historical_price[0]
   end
 
   def get_logo(symbol)
-    client = IEX::Api::Client.new(
-      publishable_token: 'Tpk_01273dfe6905454eb34261aa36cd7198',
-      secret_token: 'Tsk_c3bab9f09d4440539a75b1803a0f83a4',
-      endpoint: 'https://sandbox.iexapis.com/stable'
-    )
+    client = CallApi.call
     logo_url = client.logo(symbol)
     return logo_url.url 
+  end
+
+  def get_chart_data(stock)
+    client = CallApi.call
+    @data_keys = []
+    @data_values = []
+
+    historical_prices = client.historical_prices(stock.symbol)
+
+    historical_prices.each do |price|
+      @data_keys << price.label
+      @data_values << price.close
+    end
+
+    return { data_keys: @data_keys, data_values: @data_values }
+   
   end
 
 end
