97b0621ef49efac0181727e5d128f6e260905b8b
visnusantos1203 <visnu.santos1203@gmail.com>
2022-04-11 15:37:58 +0800

    Addes :domain => key to production.rb to try fix smtp username and password error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,6 +119,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
+    :domain               => 'https://stockscrowd.herokuapp.com/',
     :port                 => 587,
     :user_name            => Rails.application.credentials.gmail_mailer[:username],
     :password             => Rails.application.credentials.gmail_mailer[:password],
