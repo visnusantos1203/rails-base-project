8011e02eac653833da42b44478b9a2a4f67f5ef5
visnusantos1203 <visnu.santos1203@gmail.com>
2022-04-14 11:45:21 +0800

    Removes unnecessary comment

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,6 +4,20 @@ module HomeHelper
     rates.each do |rate|
       fx_hash[rate.symbol] = rate.rate
     end
-    fx_hash # lolz idk why it worked hahahahhasdasdhfasfd
+    fx_hash 
+  end
+
+  def compute_equation(formula)
+    total = formula.split("")
+    new_eq = []
+    total.each do |n|
+      if n.to_i.class? == integer
+        new_eq << n.to_i
+      else
+        new_eq << n
+      end
+      
+    end
+
   end
 end
