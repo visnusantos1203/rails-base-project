ad01a4e82bea7532b263130e136e554443002626
visnusantos1203 <visnu.santos1203@gmail.com>
2022-04-14 11:44:23 +0800

    Creates a search method that filters and return transactions based on the paramaters given

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,23 @@
 module AdminsHelper
+
+  def search(search, sort)
+    if search
+      begin
+        transactions = Payment.find(search.to_i)
+      rescue
+        flash[:alert] = "Transaction not found"
+        transactions = Payment.all
+      end
+    else
+      if sort == "transaction type"
+        transactions = Payment.order("transaction_type ASC")
+      elsif sort == "amount"
+        transactions =Payment.order("quantity * current_price DESC")
+      elsif sort == "date"
+        transactions =Payment.order("time_placed ASC")
+      else
+        transactions = Payment.all
+      end
+    end
+  end
 end
