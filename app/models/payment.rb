d1668658b10515a1f93efab4d53119988a84c4c7
visnusantos1203 <visnu.santos1203@gmail.com>
2022-04-14 11:54:14 +0800

    Creates scopes for payment model for faster performance when retrieving necessary information from the model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,4 +3,11 @@ class Payment < ApplicationRecord
   belongs_to :user
   belongs_to :stock
   belongs_to :wallet
+
+  scope :asc_time_placed, -> { order("payments.time_placed ASC")}
+  scope :desc_time_placed, -> { order("payments.time_placed DESC")}
+  scope :asc_transaction_type, -> { order("transaction_type ASC")}
+  scope :desc_transaction_type, -> { order("transaction_type DESC")}
+  scope :asc_amount, -> { order("payments.quantity * payments.current_price ASC")}
+  scope :desc_amount, -> { order("payments.quantity * payments.current_price DESC")}
 end
