2e67c60c0df8169be67c1a1de4a582a433200b64
visnusantos1203 <visnu.santos1203@gmail.com>
2022-04-15 13:49:44 +0800

    Removes block of code that has been refactored and optimized in the porfolios helper

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,17 +1,10 @@
 class PortfoliosController < ApplicationController
   before_action :authenticate_user!
-  # dito sana naka list lang lahat ng companies na may investment si user
+
   def index
-    # @payments = Payment.where(user_id: current_user.id).order('ASC')
     @payments = Payment.all.where(user_id: current_user.id)
-    @companies = []
-    @payments.each do |payment|
-      company = Stock.find_by(id: payment.stock_id)
-      @companies.push([company.company_name, payment.stock_id])
-    end
   end
 
-  # dito pwedeng ma view yung payments sa isang transaction
   def show
     @payment = Payment.find_by(params[:id])
   end
