a90474a7fbcbe4750143f15ab2a0851aaec7eb1d
visnusantos1203 <visnu.santos1203@gmail.com>
2022-05-16 13:51:07 +0800

    Adds conditional statement that filters out payments with nil stock_id

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -5,7 +5,9 @@ module PortfoliosHelper
     # Selects distinct stock ids from user's payment
     payments.distinct.pluck(:stock_id).each do |stock_id|
     # Queries the name of the company based on the distinct stock ids and then pushed into the company names array
-      sorted_company_names << [Stock.find_by(id: stock_id).company_name, stock_id]
+      unless stock_id.nil?
+        sorted_company_names << [Stock.find_by(id: stock_id).company_name, stock_id]
+      end
     end
     # Sorts and returns an array of company names
     sorted_company_names.sort
