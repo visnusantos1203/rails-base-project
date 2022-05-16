aeac1967c6ed5617ff36dab574d064b85c79403b
visnusantos1203 <visnu.santos1203@gmail.com>
2022-05-16 13:58:40 +0800

    Created class method that handles generation of csv file for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,28 @@
+require 'csv'
+
 class User < ApplicationRecord
   has_many :payments
   has_one :wallet, dependent: :destroy
 
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
   validates :first_name, :last_name, :username, presence: true
-  after_save :create_user_wallet
+  after_save :create_user_wallet #, :send_welcome_email << apparently this causes the deployed app to malfunction upon user admin approval. Removing this line of code also removes user and admin signout bug
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   scope :asc_name, -> { order("users.first_name ASC ")}
   scope :desc_name, -> { order("users.first_name DESC ")}
+
+  def self.to_csv
+    attributes = %W{id email first_name last_name username approved}
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |user|
+        csv << attributes.map { |attr| user.send(attr) }
+      end
+    end
+  end
 
   private 
 
@@ -21,8 +32,7 @@ class User < ApplicationRecord
 
   def create_user_wallet
     if wallet == nil
-      user_wallet = Wallet.new(id: id, user_id: id, balance: 100_000, created_at: Time.now, updated_at: Time.now)
-      user_wallet.save
+      user_wallet = Wallet.create!(id: id, user_id: id, balance: 100_000, created_at: Time.now, updated_at: Time.now)
     end
   end
 
