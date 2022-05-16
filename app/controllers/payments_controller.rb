e216123c5d10800c5d9b61c6ab3e2a675aac31c5
visnusantos1203 <visnu.santos1203@gmail.com>
2022-05-16 13:45:51 +0800

    Creates cashin action that integrates Stripe, adds balance checker and balance deduction functionality upon buying of stocks

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,6 +4,10 @@ class PaymentsController < ApplicationController
   def index
     @payments = Payment.all
     # @filtered_names = sorted_company_name(@payments)
+    respond_to do |format|
+      format.html
+      format.csv { send_data @payments.to_csv }
+    end
   end
 
   def show
@@ -18,27 +22,61 @@ class PaymentsController < ApplicationController
     @payment = Payment.new
   end
 
+  def new_cashin
+    @payment = Payment.new
+  end
+
+  def cashin
+    @session = StripeCashIn.perform(params[:amount], params[:currency])
+    # !! needs to have fallback option when the stripe checkout fails !! 
+    @payment = Payment.create({
+      transaction_type: "cash-in",
+      user_id: current_user.id,
+      wallet_id: current_user.id,
+      time_placed: Time.now,
+      quantity: 1,
+      current_price: params[:amount]
+    })
+    PaymentHandler.top_up(params[:amount].to_i, current_user.id)
+
+    respond_to do |format|
+      format.js
+    end
+    
+    
+    
+
+  end
+
   def create
     client = CallApi.call
     stocks = Stock.all
+    user_id = current_user.id
     @payment = Payment.new(payment_params)
-    @payment.user_id = current_user.id
-    @payment.wallet_id = current_user.id # value needs to be replaced with dynamic user_id
-    @payment.time_placed = Time.now
-    @payment.current_price = client.price(stocks[@payment.stock_id].symbol)
-    
-    if @payment.save
-      redirect_to portfolios_path
-    else
-      puts params
-      redirect_back fallback_location: payments_path
-    end
-  end
+    current_price = client.price(stocks[@payment.stock_id].symbol)
+    amount = (payment_params[:quantity].to_i * current_price)
 
+    if BalanceChecker.enough_balance(amount, user_id) == true
+      @payment.current_price = current_price
+
+      if @payment.save
+        PaymentHandler.deduct_payment(amount, user_id)
+
+        redirect_to portfolios_path
+      else
+        puts "Your transaction did not push through"
+        redirect_back fallback_location: payments_path
+      end
+    else
+      redirect_back fallback_location: stocks_path
+      puts "Insufficient balance"
+    end
+
+  end
 
   private
 
   def payment_params
-    params.require(:payment).permit(:quantity, :stock_id, :transaction_type)
+    params.require(:payment).permit(:quantity, :stock_id, :transaction_type, :user_id, :wallet_id, :time_placed)
   end
 end
